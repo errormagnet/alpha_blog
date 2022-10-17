7aff02232cad449c9c21fd01f3dce29b72516767
sid420 <sawan.nepal@selise.ch>
2022-10-17 13:50:48 +0600

    feat: users & pagination

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
+# Pagination
+gem 'will_paginate', '~> 3.3'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,36 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @user = User.all
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = 'Account successfully created. Welcome.'
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'new'
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Profile Updated'
+      redirect_to user_path
+    else
+      redirect_to user_path
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
